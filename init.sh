47c560f562d0c941d1f37161a04b00a6fa49b4b7
mirman <matthew.mirman@inf.ethz.ch>
2017-08-15 16:37:10 +0200

    fixed the init script

--- init.sh
+++ init.sh
@@ -1,15 +1,9 @@
+#!/bin/bash
+
 git submodule update --init --recursive
 
-cabal sandbox init
-cabal sandbox add-source tensorflow-haskell/tensorflow-proto
-cabal sandbox add-source tensorflow-haskell/tensorflow-queue
-cabal sandbox add-source tensorflow-haskell/tensorflow-ops
-cabal sandbox add-source tensorflow-haskell/tensorflow-opgen
-cabal sandbox add-source tensorflow-haskell/tensorflow-logging
-cabal sandbox add-source tensorflow-haskell/tensorflow-mnist
-cabal sandbox add-source tensorflow-haskell/tensorflow-mnist-input-data
-cabal sandbox add-source tensorflow-haskell/tensorflow-records
-cabal sandbox add-source tensorflow-haskell/tensorflow-records-conduit
-cabal sandbox add-source tensorflow-haskell/tensorflow
+./tensorflow-haskell/tools/install_macos_dependencies.sh
 
-cabal install --dependencies-only
+cabal install tensorflow-haskell/tensorflow tensorflow-haskell/tensorflow-proto tensorflow-haskell/tensorflow-opgen/ tensorflow-haskell/tensorflow-ops tensorflow-haskell/tensorflow-core-ops tensorflow-haskell/tensorflow-logging/ tensorflow-haskell/tensorflow-records tensorflow-haskell/tensorflow-records-conduit
+
+cabal install
