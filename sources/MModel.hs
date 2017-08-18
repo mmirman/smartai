6bd2bfee46e667d0f189a61235c039bcc79e2b6d
mirman <matthew.mirman@inf.ethz.ch>
2017-08-18 17:20:27 +0200

    most of simpleTrainModel is done

--- sources/MModel.hs
+++ sources/MModel.hs
@@ -31,10 +31,15 @@ instance MonadBuild (WriterT [FVar] Build) where
 type Input v = FNode
 type Output v = FNode
 
-data Model i = Model { modelInputs    :: [FVar]
-                     , modelVariables :: [FVar]
-                     , modelOutputs   :: [FNode]
-                     }
+data Model = Model { modelInputs    :: [FVar]
+                   , modelVariables :: [FVar]
+                   , modelOutputs   :: [FNode]
+                   }
+
+newtype MModel a = MModel { unMModel :: forall m . MonadBuild m => m Model }
+
+mmodel :: Model -> MModel a
+mmodel a = MModel $ return a
 
 data Phant a = Phant
 
@@ -43,30 +48,30 @@ class Encodable a where
   encodeInTensor :: a -> TensorData Float
   
 class BuildMagic i o | i -> o where
-  buildWriter :: MonadBuild m => i -> m (Model o)
+  buildWriter :: i -> MModel o
 
 instance BuildMagic (WriterBuild (Output a)) a where
-  buildWriter modelBuild = do
+  buildWriter modelBuild = MModel $ do
     (out, vars) <- build $ runWriterT modelBuild
     return $ Model [] vars [out]
 
 instance BuildMagic (WriterBuild [Output a]) [a] where
-  buildWriter modelBuild = do
+  buildWriter modelBuild = MModel $ do
     (out, vars) <- build $ runWriterT modelBuild
     return $ Model [] vars out
 
 instance (BuildMagic (WriterBuild a) a', BuildMagic (WriterBuild b) b') => BuildMagic (WriterBuild (a, b)) (a',b') where
-  buildWriter modelBuild = do -- this is only sound if modelBuild doesn't do IO.
+  buildWriter modelBuild = MModel $ do -- this is only sound if modelBuild doesn't do IO.
     ((out1,out2), vars) <- build $ runWriterT modelBuild
-    Model _ _ o1 <- buildWriter (return out1 :: WriterBuild a)
-    Model _ _ o2 <- buildWriter (return out2 :: WriterBuild b)
+    Model _ _ o1 <- unMModel $ buildWriter (return out1 :: WriterBuild a)
+    Model _ _ o2 <- unMModel $ buildWriter (return out2 :: WriterBuild b)
     return $ Model [] vars $ o1 ++ o2
 
 instance (Encodable a, BuildMagic r e) => BuildMagic (Input a -> r) (a -> e) where
-  buildWriter modelBuild = do
+  buildWriter modelBuild = MModel $ do
     inp <- variable $ getEncodingShape (Phant :: Phant a)
     vinp <- renderValue $ readValue inp
-    Model inps vs out <- buildWriter $ modelBuild vinp
+    Model inps vs out <- unMModel $ buildWriter $ modelBuild vinp
     return $ Model (inp:inps) vs out
 
 
@@ -86,38 +91,54 @@ initializedRandomParam :: Int64 -> Shape -> WriterBuild FNode
 initializedRandomParam len shape = initializedParam =<< randomParam len shape
 
 -- the result can be destroyed by using the second model elsewhere!!
-compose :: MonadBuild m => Model (a -> b) -> Model (b -> c) -> m (Model (a -> c))
-compose (Model ins vars outs)  (Model ins' vars' outs') = do
+compose :: MModel (a -> b) -> MModel (b -> c) -> MModel (a -> c)
+compose m1 m2 = MModel $ do
+  Model ins vars outs <- unMModel m1
+  Model ins' vars' outs' <- unMModel m2
   sequence_ $ zipWith assign ins' outs
   return $ Model ins (vars ++ vars') outs'
+
+apply :: MModel a -> MModel (a -> b) ->  MModel b
+apply m1 m2 = MModel $ do
+  Model ins vars outs <- unMModel m1
+  Model ins' vars' outs' <- unMModel m2
+  sequence_ $ zipWith assign ins' outs
+  return $ Model ins (vars ++ vars') outs'  
 
 ---------------------
 -- Training Models --
 ---------------------
 
-applyVal :: (Encodable a) => Model (a -> b) -> a -> (Feed, Model b)
-applyVal (Model (i:r) vs outs) a = (feed i $ encodeInTensor a, Model r vs outs)
+applyVal :: (Encodable a, MonadBuild m) => MModel (a -> b) -> a -> m (Feed, MModel b)
+applyVal m1 a = do
+  Model (i:r) vs outs <- unMModel m1
+  return (feed i $ encodeInTensor a, mmodel $ Model r vs outs)
+
 
 class ApplyVals a r b | a r -> b where
-  applyVals :: Model a -> r -> ([Feed], Model b)
+  applyVals :: MonadBuild m => MModel a -> r -> m ([Feed], MModel b)
 instance ApplyVals a () a where
-  applyVals m () = ([], m)
+  applyVals m () = return ([], m)
 instance (Encodable a, ApplyVals b r c) => ApplyVals (a -> b) (a,r) c where
-  applyVals (Model (i:r) vs outs) (a,rest) = (feed i (encodeInTensor a):fd, m)
-    where (fd, m) = applyVals (Model r vs outs :: Model b) rest
+  applyVals m (a,rest) = do
+    Model (i:r) vs outs <- unMModel m
+    (fd, m) <- applyVals (mmodel $ Model r vs outs :: MModel b) rest    
+    return (feed i (encodeInTensor a):fd, m)
 
-simpleTrainModel :: (ApplyVals a i o, Encodable a, Encodable o, MonadBuild m) => Model (a -> o) -> [(i, o)] -> Model (o -> o -> Float) {- error function -} -> m (Model a)
-simpleTrainModel totrain dataset loss_function = do
-  let feeder ins = runWithFeeds feeds 
-        where (feeds, Model [] weights outs) = applyVals totrain ins
-              --(feeds, Model [] weights outs) = applyVals totrain ins
+simpleTrainModel :: forall a i o v . (ApplyVals a i o, Encodable a, Encodable o) => MModel a -> [(i, o)] -> MModel (o -> o -> Float) -> (forall m . MonadBuild m => FNode -> [Variable Float] -> m ControlNode) -> MModel a
+simpleTrainModel totrain dataset loss_model minimizer = MModel $ do
+  let feeder :: i -> o -> Session ()
+      feeder ins label = do
+        (feeds, mo) <- applyVals totrain ins
+        (feed, final_loss) <- applyVal (apply mo loss_model) label
+        Model [] vars [out] <- unMModel final_loss
+        trainStep <- minimizer out vars
+        runWithFeeds_ feeds trainStep
   undefined
 
 ------------------
 -- Example code --
 ------------------
-buildModelEx :: forall a m . (Encodable a, MonadBuild m) => m (Model (a -> ()))
+buildModelEx :: forall a m . Encodable a => MModel (a -> ())
 buildModelEx = buildWriter $ \(input_one :: Input a) -> do -- should get converted automatically into placeholders.
   returnOutput undefined
-
-
